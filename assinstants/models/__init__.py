0b755b07e4bcaca46abd461b8d279711f9a2447e
Lahfir <nmhlahfir2@gmail.com>
2024-09-30 20:27:01 -0400

    fix: added init for models

---
+++ assinstants/models/__init__.py
@@ -0,0 +1,9 @@
+import assistant
+import thread
+import run
+import tool
+import function
+import message
+import shared
+
+__all__ = ["assistant", "thread", "run", "tool", "function", "message", "shared"]
